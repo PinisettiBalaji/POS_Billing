c2c3df5ef3d90854cb028d09f40ff3b3ea19bdc5
Pinisetti Balaji <pinisetti.balaji8@gmail.com>
2024-11-01 22:48:21 +0530

    MongoDB connection setup

--- server.js
+++ server.js
@@ -4,5 +4,14 @@ const mongoose = require("mongoose")
 const app=express();
 const port=7000;
 
-const mongodburl="mongodb://localhost:27017/posdb"
+const mongodburl="mongodb+srv://pinisettibalaji8:Balaji%40Preethi143@poscluster.rhzmx.mongodb.net/"
 mongoose.connect(mongodburl);
+
+const connection= mongoose.connection
+app.listen(port,()=>{
+    console.log("Server is Running at port ", port)
+});
+
+connection.once("open",()=>{
+    console.log("Mongodb connected!!!");
+})
