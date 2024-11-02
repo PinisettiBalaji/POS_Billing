4dd3515e6b03be77594781cabebba55ead366a57
Pinisetti Balaji <pinisetti.balaji8@gmail.com>
2024-11-02 18:41:11 +0530

    sales model added

---
+++ models/salesDetailsModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+var Schema = mongoose.Schema;
+
+var salesDetailsSchema = new Schema(
+    {
+        salesId: {
+            type: mongoose.Types.ObjectId,
+            ref: 'Sales',
+            required: true
+        },
+        items: [
+            {
+                productName: {
+                    type: String,
+                    required: true
+                },
+                price: {
+                    type: Number,
+                    required: true
+                },
+                quantity: {
+                    type: Number,
+                    required: true
+                },
+                total: {
+                    type: Number,
+                    required: true
+                }
+            }
+        ]
+    }
+)
+
+salesDetailsSchema.plugin(require('mongoose-autopopulate'));
+
+const SalesDetails = mongoose.model("SalesDetails", salesDetailsSchema);
+module.exports = SalesDetails;
+
