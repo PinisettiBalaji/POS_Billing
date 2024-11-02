91fc9e0658ea0dccee6c4da067fa3e3da5cf3295
Pinisetti Balaji <pinisetti.balaji8@gmail.com>
2024-11-02 18:32:46 +0530

    added server settings

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
-const express = require("express")
-const mongoose = require("mongoose")
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
-const app=express();
-const port=7000;
+//routes
+var categoryroutes = require("./routes/category");
+var brandroutes = require("./routes/brand");
+var productroutes = require("./routes/product");
 
-const mongodburl="mongodb+srv://pinisettibalaji8:Balaji%40Preethi143@poscluster.rhzmx.mongodb.net/"
+const app = express();
+const port = 7000;
+const mongodburl = "mongodb://0.0.0.0:27017/posdb";
+// const mongodburl ="mongodb+srv://pinisettibalaji8:Balaji%40Preethi143@cluster0.rhzmx.mongodb.net/";
 mongoose.connect(mongodburl);
 
-const connection= mongoose.connection
-app.listen(port,()=>{
-    console.log("Server is Running at port ", port)
+const connection = mongoose.connection;
+app.listen(port, () => {
+  console.log("Server is Running at port ", port);
 });
 
-connection.once("open",()=>{
-    console.log("Mongodb connected!!!");
-})
+connection.once("open", () => {
+  console.log("Mongodb connected!!!");
+});
+
+app.use(bodyParser.json());
+app.use(categoryroutes);
+app.use(brandroutes);
+app.use(productroutes);
